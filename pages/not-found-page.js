4a0f6b8dc5e9790d5487273e93915680149ff3eb
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-06-08 09:06:18 +0100

    update

---
+++ pages/not-found-page.js
@@ -0,0 +1,3 @@
+export default function notFound(){
+	document.body.innerHTML = '<h2> Requested page doesn\'t exist</h2>'
+}
---
+++ router.js
@@ -0,0 +1,18 @@
+/*import loginPage from './pages/loginPage.js';
+import signupPage from './pages/signupPage.js';*/
+import home from './pages/home-page.js'
+import notFound from './pages/not-found-page.js'
+
+/*const routes = {
+	'/': home
+}
+
+const router = function(){
+	const route = location.hash.slice(1) || '/'
+	const page = routes[route] || notFound
+	page()
+	lucide.createIcons()
+}
+
+window.addEventListener('hashchange', router)
+window.addEventListener('load', router)*/
