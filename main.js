6d1a6e2bc1256ca12a745af77d66096be1fa49ad
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-06-13 07:06:31 +0100

    update main.js

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ import { router } from './router.js'
 window.addEventListener('popstate', router.route)
 //window.addEventListener('hashchange', router.route)
 window.addEventListener('load', router.route)
-//history.pushState(null, '', '/#/login')
+//history.pushState(null, '', '/login')
