7c2a32fed0af4a90de6acfe9a53a0fc8e9808f42
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-07-04 08:08:34 +0100

    update

---
+++ dom-manager.js
@@ -0,0 +1,41 @@
+class DomManager{
+	constructor(){
+		this.chatPreviewDom = {}
+		this.chatDom = {}
+	}
+	
+	createChatPreviewDom(key, domElement){
+		this.chatPreviewDom[key] = domElement
+	}
+	
+	createChatDom(key, domElement){
+		this.chatDom[key] = domElement
+	}
+	
+	updateChatPreview(domKey, callback){
+		const domElement = this.chatPreviewDom[domKey]
+		callback(domElement)
+	}
+	
+	updateChat(domKey, callback){
+		const domElement = this.chatDom[domKey]
+		callback(domElement)
+	}
+	
+	deleteChatPreviewDomElement(key){
+		this.chatPreviewDom[key] = undefined
+	}
+
+	deleteChatDomElement(key){
+		this.chatPreviewDom[key] = undefined
+	}
+
+	clearChatPreviewDomElement(){
+		this.chatPreviewDom.clear()
+	}
+	
+	clearChatDomElement(){
+		this.chatDom.clear()
+	}
+	
+}
--- socket.js
+++ socket.js
@@ -2,18 +2,38 @@ import server from './fetch.js';
 
 class Socket {
 	constructor(baseUrl = 'wss://beep-me-api.onrender.com/ws/') {
-		this.chatsocket = new WebSocket(`${baseUrl}chat/?token=${server.getAccessToken()}`)
-		this.notificationSocket = new WebSocket(`${baseUrl}notification/?token=${server.getAccessToken()}`)
-		this.maxRetry = 5
-		this.retryCount = 0
+		this.chatsocket = null
+		this.notificationSocket = null
+		this.MaxRetry = 5
+		this.notificationRetryCount = 0
+		this.chatRetryCount = 0
 	}
 	
 	connect(){
+		this.chatsocket = new WebSocket(`${baseUrl}chat/?token=${server.getAccessToken()}`)
+		this.notificationSocket = new WebSocket(`${baseUrl}notification/?token=${server.getAccessToken()}`)
 		this.chatsocket.onclose = (event) => {
-			if (!(event.code === 1000 || event.code === 1001) && this.retryCount >= this.maxRetry){
+			if (!(event.code === 1000 || event.code === 1001) && this.chatRetryCount >= this.maxRetry){
 			    this.chatsocket = new WebSocket(`${baseUrl}chat/?token=${server.getAccessToken()}`)
+			    this.chatRetryCount ++
+			    console.warn('Chat socket closed retrying connection')
 			}
 		}
+		this.chatsocket.onopen = () => {
+			this.chatRetryCount = 0
+		}
+		
+		this.notificationSocket.onclose = (event) => {
+			if (!(event.code === 1000 || event.code === 1001) && this.notificationRetryCount >= this.maxRetry){
+			    this.notificationSocket = new WebSocket(`${baseUrl}notification/?token=${server.getAccessToken()}`)
+			    this.notificationRetryCount ++
+			    console.warn('Notification socket closed retrying connection')
+			}
+		}
+		this.notificationSocket.onopen = () => {
+			this.notificationRetryCount = 0
+		}
+		
 	}
 	
 }
