73f70aecade608d5c7d439636bc242d93afb755b
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-07-11 10:35:20 +0100

    made the message input clear on sending

--- pages/chat-page.js
+++ pages/chat-page.js
@@ -111,6 +111,7 @@ sendBtn.addEventListener('click', () => {
 	const newMessageBubbleDiv = chatBubble(true, message, 'pending', temporary_id)
 	domManager.getChatDom(friend_username).push(newMessageBubbleDiv)
 	messageMainDiv.appendChild(newMessageBubbleDiv)
+	messageInput.value = ''
 })
 
 messageInputDiv.appendChild(sendBtn)
