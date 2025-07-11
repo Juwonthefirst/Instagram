4674eb03a5b17e91019f159028ab3b0a2620b613
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-07-12 00:05:27 +0100

    fixed submit bug

--- pages/signup-page.js
+++ pages/signup-page.js
@@ -110,7 +110,6 @@ const onSignupError = (data) => {
 form.addEventListener('submit', async (event) => {
 	event.preventDefault()
 	if (formValidator.validate()) {
-	
 		const email = emailInput.value.trim()
 		const password = passInput.value.trim()
 		const username = email.replace('@gmail.com', String(Math.random() * 10000))
