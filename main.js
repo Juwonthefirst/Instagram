62ffc4706b80efb4684ee01f7e1f86b26e1f949f
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-07-13 11:00:15 +0100

    bug fixes

--- main.js
+++ main.js
@@ -8,18 +8,16 @@ window.addEventListener('popstate', router.route)
 window.addEventListener('load', async () => {
 	const response = await server.get_csrf()
 	if (response.error) { return await router.navigateTo('/login') }
-	else {
-		await server.startAutoRefreshAccessToken()
-		alert('it continues')
-		if (server.getAccessToken()) {
-			await server.getUser({
-				onSuccess: (data) => memory.setCurrentUser(data)
-			})
-			
-			socket.connect()
-			socket.listenForNotifications()
-		}
+	await server.startAutoRefreshAccessToken()
+	alert('it continues')
+	if (server.getAccessToken()) {
+		await server.getUser({
+			onSuccess: (data) => memory.setCurrentUser(data)
+		})
 		
+		socket.connect()
+		socket.listenForNotifications()
 	}
+	
 	await router.route()
 })
